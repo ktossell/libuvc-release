af5bb715a1870890fd185c49088f45b5100c398f
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2018-04-13 16:01:10 +0900

    Rebase from 'debian/kinetic/libuvc'

--- src/ctrl-gen.py
+++ src/ctrl-gen.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
 from __future__ import print_function
+from collections import OrderedDict
+import getopt
 import sys
+import yaml
+
+class quoted(str): pass
+
+def quoted_presenter(dumper, data):
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='"')
+yaml.add_representer(quoted, quoted_presenter)
+
+class literal(str): pass
+
+def literal_presenter(dumper, data):
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+yaml.add_representer(literal, literal_presenter)
+
+def ordered_dict_presenter(dumper, data):
+    return dumper.represent_dict(data.items())
+yaml.add_representer(OrderedDict, ordered_dict_presenter)
+
+def dict_constructor(loader, node):
+    return OrderedDict(loader.construct_pairs(node))
+_mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+yaml.add_constructor(_mapping_tag, dict_constructor)
 
 class IntField(object):
     def __init__(self, name, position, length, signed):
@@ -36,185 +60,37 @@ class IntField(object):
         elif self.length == 4:
             return "INT_TO_DW({0}, data + {1});".format(self.name, self.position)
 
-CT_CONTROLS = [
-    ['scanning_mode', 'SCANNING_MODE', 1,
-     [(IntField('mode', 0, 1, False),
-       '0: interlaced, 1: progressive')]],
-    ['ae_mode', 'AE_MODE', 1,
-     [(IntField('mode', 0, 1, False),
-       'TODO')]],
-    ['ae_priority', 'AE_PRIORITY', 1,
-     [(IntField('priority', 0, 1, False),
-       'TODO')]],
-    ['exposure_abs', 'EXPOSURE_TIME_ABSOLUTE', 4,
-     [(IntField('time', 0, 4, False),
-       'TODO')]],
-    ['exposure_rel', 'EXPOSURE_TIME_RELATIVE', 1,
-     [(IntField('step', 0, 1, True),
-       'TODO')]],
-    ['focus_abs', 'FOCUS_ABSOLUTE', 2,
-     [(IntField('focus', 0, 2, False),
-       'TODO')]],
-    ['focus_rel', 'FOCUS_RELATIVE', 2,
-     [(IntField('focus_rel', 0, 1, True),
-       'TODO'),
-      (IntField('speed', 1, 1, False),
-       'TODO')]],
-    ['focus_simple_range', 'FOCUS_SIMPLE', 1,
-     [(IntField('focus', 0, 1, False),
-       'TODO')]],
-    ['focus_auto', 'FOCUS_AUTO', 1,
-     [(IntField('state', 0, 1, False),
-       'TODO')]],
-    ['iris_abs', 'IRIS_ABSOLUTE', 2,
-     [(IntField('iris', 0, 2, False),
-       'TODO')]],
-    ['iris_rel', 'IRIS_RELATIVE', 1,
-     [(IntField('iris_rel', 0, 1, False),
-       'TODO')]],
-    ['zoom_abs', 'ZOOM_ABSOLUTE', 2,
-     [(IntField('focal_length', 0, 2, False),
-       'TODO')]],
-    ['zoom_rel', 'ZOOM_RELATIVE', 3,
-     [(IntField('zoom_rel', 0, 1, True),
-       'TODO'),
-      (IntField('digital_zoom', 1, 1, False),
-       'TODO'),
-      (IntField('speed', 2, 1, False),
-       'TODO')]],
-    ['pantilt_abs', 'PANTILT_ABSOLUTE', 8,
-     [(IntField('pan', 0, 4, True),
-       'TODO'),
-      (IntField('tilt', 4, 4, True),
-       'TODO')]],
-    ['pantilt_rel', 'PANTILT_RELATIVE', 4,
-     [(IntField('pan_rel', 0, 1, True),
-       'TODO'),
-      (IntField('pan_speed', 1, 1, False),
-       'TODO'),
-      (IntField('tilt_rel', 2, 1, True),
-       'TODO'),
-      (IntField('tilt_speed', 3, 1, False),
-       'TODO')]],
-    ['roll_abs', 'ROLL_ABSOLUTE', 2,
-     [(IntField('roll', 0, 2, True),
-       'TODO')]],
-    ['roll_rel', 'ROLL_RELATIVE', 2,
-     [(IntField('roll_rel', 0, 1, True),
-       'TODO'),
-      (IntField('speed', 1, 1, False),
-       'TODO')]],
-    ['privacy', 'PRIVACY', 1,
-     [(IntField('privacy', 0, 1, False),
-       'TODO')]],
-    ['digital_window', 'DIGITAL_WINDOW', 12,
-     [(IntField('window_top', 0, 2, False),
-       'TODO'),
-      (IntField('window_left', 2, 2, False),
-       'TODO'),
-      (IntField('window_bottom', 4, 2, False),
-       'TODO'),
-      (IntField('window_right', 6, 2, False),
-       'TODO'),
-      (IntField('num_steps', 8, 2, False),
-       'TODO'),
-      (IntField('num_steps_units', 10, 2, False),
-       'TODO')]],
-    ['digital_roi', 'REGION_OF_INTEREST', 10,
-     [(IntField('roi_top', 0, 2, False),
-       'TODO'),
-      (IntField('roi_left', 2, 2, False),
-       'TODO'),
-      (IntField('roi_bottom', 4, 2, False),
-       'TODO'),
-      (IntField('roi_right', 6, 2, False),
-       'TODO'),
-      (IntField('auto_controls', 8, 2, False),
-       'TODO')]],
-]
+    def spec(self):
+        rep = [('position', self.position), ('length', self.length)]
+        if self.signed:
+            rep.append(('signed', True))
+        return rep
 
-SU_CONTROLS = [
-    ['input_select', 'INPUT_SELECT', 1,
-     [(IntField('selector', 0, 1, False),
-       'TODO')]],
-]
+    @staticmethod
+    def load(spec):
+        return IntField(spec['name'], spec['position'], spec['length'], spec['signed'] if signed in spec else False)
 
-PU_CONTROLS = [
-    ['backlight_compensation', 'BACKLIGHT_COMPENSATION', 2,
-     [(IntField('backlight_compensation', 0, 2, False),
-       'device-dependent backlight compensation mode;' +
-       'zero means backlight compensation is disabled'),
-     ]],
-    ['brightness', 'BRIGHTNESS', 2,
-     [(IntField('brightness', 0, 2, True),
-       'TODO')]],
-    ['contrast', 'CONTRAST', 2,
-     [(IntField('contrast', 0, 2, False),
-       'TODO')]],
-    ['contrast_auto', 'CONTRAST_AUTO', 1,
-     [(IntField('contrast_auto', 0, 1, False),
-       'TODO')]],
-    ['gain', 'GAIN', 2,
-     [(IntField('gain', 0, 2, False),
-       'TODO')]],
-    ['power_line_frequency', 'POWER_LINE_FREQUENCY', 1,
-     [(IntField('power_line_frequency', 0, 1, False),
-       'TODO')]],
-    ['hue', 'HUE', 2,
-     [(IntField('hue', 0, 2, True),
-       'TODO')]],
-    ['hue_auto', 'HUE_AUTO', 1,
-     [(IntField('hue_auto', 0, 1, False),
-       'TODO')]],
-    ['saturation', 'SATURATION', 2,
-     [(IntField('saturation', 0, 2, False),
-       'TODO')]],
-    ['sharpness', 'SHARPNESS', 2,
-     [(IntField('sharpness', 0, 2, False),
-       'TODO')]],
-    ['gamma', 'GAMMA', 2,
-     [(IntField('gamma', 0, 2, False),
-       'TODO')]],
-    ['white_balance_temperature', 'WHITE_BALANCE_TEMPERATURE', 2,
-     [(IntField('temperature', 0, 2, False),
-       'TODO')]],
-    ['white_balance_temperature_auto', 'WHITE_BALANCE_TEMPERATURE_AUTO', 1,
-     [(IntField('temperature_auto', 0, 1, False),
-       'TODO')]],
-    ['white_balance_component', 'WHITE_BALANCE_COMPONENT', 4,
-     [(IntField('blue', 0, 2, False),
-       'TODO'),
-      (IntField('red', 2, 2, False),
-       'TODO')]],
-    ['white_balance_component_auto', 'WHITE_BALANCE_COMPONENT_AUTO', 1,
-     [(IntField('white_balance_component_auto', 0, 1, False),
-       'TODO')]],
-    ['digital_multiplier', 'DIGITAL_MULTIPLIER', 2,
-     [(IntField('multiplier_step', 0, 2, False),
-       'TODO')]],
-    ['digital_multiplier_limit', 'DIGITAL_MULTIPLIER_LIMIT', 2,
-     [(IntField('multiplier_step', 0, 2, False),
-       'TODO')]],
-    ['analog_video_standard', 'ANALOG_VIDEO_STANDARD', 1,
-     [(IntField('video_standard', 0, 1, False),
-       'TODO')]],
-    ['analog_video_lock_status', 'ANALOG_LOCK_STATUS', 1,
-     [(IntField('status', 0, 1, False),
-       'TODO')]],
-]
+def load_field(name, spec):
+    if spec['type'] == 'int':
+        return IntField(name, spec['position'], spec['length'], spec.get('signed', False))
+    else:
+        raise Exception("unknown field type '{0}'".format(spec['type']))
 
-# @todo Encoding unit controls
-
-GETTER_TEMPLATE = """/** @ingroup ctrl */
-uvc_error_t uvc_get_{function_name}(uvc_device_handle_t *devh, {args_signature}, enum uvc_req_code req_code) {{
+GETTER_TEMPLATE = """/** @ingroup ctrl
+ * {gen_doc}
+ * @param devh UVC device handle
+ * {args_doc}
+ * @param req_code UVC_GET_* request to execute
+ */
+uvc_error_t uvc_get_{control_name}(uvc_device_handle_t *devh, {args_signature}, enum uvc_req_code req_code) {{
   uint8_t data[{control_length}];
   uvc_error_t ret;
 
   ret = libusb_control_transfer(
     devh->usb_devh,
     REQ_TYPE_GET, req_code,
-    UVC_{unit}_{control_name}_CONTROL << 8,
-    1 << 8,
+    {control_code} << 8,
+    {unit_fn} << 8 | devh->info->ctrl_if.bInterfaceNumber,
     data,
     sizeof(data),
     0);
@@ -228,8 +104,12 @@ uvc_error_t uvc_get_{function_name}(uvc_device_handle_t *devh, {args_signature},
 }}
 """
 
-SETTER_TEMPLATE = """/** @ingroup ctrl */
-uvc_error_t uvc_set_{function_name}(uvc_device_handle_t *devh, {args_signature}) {{
+SETTER_TEMPLATE = """/** @ingroup ctrl
+ * {gen_doc}
+ * @param devh UVC device handle
+ * {args_doc}
+ */
+uvc_error_t uvc_set_{control_name}(uvc_device_handle_t *devh, {args_signature}) {{
   uint8_t data[{control_length}];
   uvc_error_t ret;
 
@@ -238,8 +118,8 @@ uvc_error_t uvc_set_{function_name}(uvc_device_handle_t *devh, {args_signature})
   ret = libusb_control_transfer(
     devh->usb_devh,
     REQ_TYPE_SET, UVC_SET_CUR,
-    UVC_{unit}_{control_name}_CONTROL << 8,
-    1 << 8,
+    {control_code} << 8,
+    {unit_fn} << 8 | devh->info->ctrl_if.bInterfaceNumber,
     data,
     sizeof(data),
     0);
@@ -251,44 +131,153 @@ uvc_error_t uvc_set_{function_name}(uvc_device_handle_t *devh, {args_signature})
 }}
 """
 
-def gen_decl(unit, function_name, control_name, control_length, fields):
+def gen_decl(unit_name, unit, control_name, control):
+    fields = [(load_field(field_name, field_details), field_details['doc']) for field_name, field_details in control['fields'].items()] if 'fields' in control else []
+
     get_args_signature = ', '.join([field.getter_sig() for (field, desc) in fields])
     set_args_signature = ', '.join([field.setter_sig() for (field, desc) in fields])
 
     return "uvc_error_t uvc_get_{function_name}(uvc_device_handle_t *devh, {args_signature}, enum uvc_req_code req_code);\n".format(**{
-        "function_name": function_name,
+        "function_name": control_name,
         "args_signature": get_args_signature
     }) + "uvc_error_t uvc_set_{function_name}(uvc_device_handle_t *devh, {args_signature});\n".format(**{
-        "function_name": function_name,
+        "function_name": control_name,
         "args_signature": set_args_signature
     })
 
-def gen_ctrl(unit, function_name, control_name, control_length, fields):
+def gen_ctrl(unit_name, unit, control_name, control):
+    fields = [(load_field(field_name, field_details), field_details['doc']) for field_name, field_details in control['fields'].items()] if 'fields' in control else []
+
     get_args_signature = ', '.join([field.getter_sig() for (field, desc) in fields])
     set_args_signature = ', '.join([field.setter_sig() for (field, desc) in fields])
     unpack = "\n    ".join([field.unpack() for (field, desc) in fields])
     pack = "\n  ".join([field.pack() for (field, desc) in fields])
 
-    return GETTER_TEMPLATE.format(**{
-        "unit": unit,
-        "function_name": function_name,
-        "control_name": control_name,
-        "control_length": control_length,
-        "args_signature": get_args_signature,
-        "unpack": unpack,
-    }) + "\n\n" + SETTER_TEMPLATE.format(**{
-        "unit": unit,
-        "function_name": function_name,
-        "control_name": control_name,
-        "control_length": control_length,
-        "args_signature": set_args_signature,
-        "pack": pack,
-    })
+    get_gen_doc_raw = None
+    set_gen_doc_raw = None
 
+    if 'doc' in control:
+        doc = control['doc']
 
+        if isinstance(doc, str):
+            get_gen_doc_raw = "\n * ".join(doc.splitlines())
+            set_gen_doc_raw = get_gen_doc_raw
+        else:
+            if 'get' in doc:
+                get_gen_doc_raw = "\n * ".join(doc['get'].splitlines())
+            if 'set' in doc:
+                set_gen_doc_raw = "\n * ".join(doc['set'].splitlines())
+
+    if get_gen_doc_raw is not None:
+        get_gen_doc = get_gen_doc_raw.format(gets_sets='Reads')
+    else:
+        get_gen_doc = '@brief Reads the ' + control['control'] + ' control.'
+
+    if set_gen_doc_raw is not None:
+        set_gen_doc = set_gen_doc_raw.format(gets_sets='Sets')
+    else:
+        set_gen_doc = '@brief Sets the ' + control['control'] + ' control.'
+
+    get_args_doc = "\n * ".join(["@param[out] {0} {1}".format(field.name, desc) for (field, desc) in fields])
+    set_args_doc = "\n * ".join(["@param {0} {1}".format(field.name, desc) for (field, desc) in fields])
+
+    control_code = 'UVC_' + unit['control_prefix'] + '_' + control['control'] + '_CONTROL'
+
+    unit_fn = "uvc_get_camera_terminal(devh)->bTerminalID" if (unit_name == "camera_terminal") else ("uvc_get_" + unit_name + "s(devh)->bUnitID")
+
+    return GETTER_TEMPLATE.format(
+        unit=unit,
+        unit_fn=unit_fn,
+        control_name=control_name,
+        control_code=control_code,
+        control_length=control['length'],
+        args_signature=get_args_signature,
+        args_doc=get_args_doc,
+        gen_doc=get_gen_doc,
+        unpack=unpack) + "\n\n" + SETTER_TEMPLATE.format(
+            unit=unit,
+            unit_fn=unit_fn,
+            control_name=control_name,
+            control_code=control_code,
+            control_length=control['length'],
+            args_signature=set_args_signature,
+            args_doc=set_args_doc,
+            gen_doc=set_gen_doc,
+            pack=pack
+        )
+
+def export_unit(unit):
+    def fmt_doc(doc):
+        def wrap_doc_entry(entry):
+            if "\n" in entry:
+                return literal(entry)
+            else:
+                return entry
+
+        if isinstance(doc, str):
+            return wrap_doc_entry(doc)
+        else:
+            return OrderedDict([(mode, wrap_doc_entry(text)) for mode, text in doc.items()])
+
+    def fmt_ctrl(control_name, control_details):
+        contents = OrderedDict()
+        contents['control'] = control_details['control']
+        contents['length'] = control_details['length']
+        contents['fields'] = control_details['fields']
+
+        if 'doc' in control_details:
+            contents['doc'] = fmt_doc(control_details['doc'])
+
+        return (control_name, contents)
+
+    unit_out = OrderedDict()
+    unit_out['type'] = unit['type']
+    if 'guid' in unit:
+        unit_out['guid'] = unit['guid']
+    if 'description' in unit:
+        unit_out['description'] = unit['description']
+    if 'control_prefix' in unit:
+        unit_out['control_prefix'] = unit['control_prefix']
+    unit_out['controls'] = OrderedDict([fmt_ctrl(ctrl_name, ctrl_details) for ctrl_name, ctrl_details in unit['controls'].items()])
+    return unit_out
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'def':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:", ["help", "input="])
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+        sys.exit(-1)
+
+    inputs = []
+
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+        elif opt in ('-i', '--input'):
+            inputs.append(val)
+
+    mode = None
+    for arg in args:
+        if arg in ('def', 'decl', 'yaml'):
+            if mode is None:
+                mode = arg
+            else:
+                print("Can't specify more than one mode")
+                sys.exit(-1)
+        else:
+            print("Invalid mode '{0}'".format(arg))
+            sys.exit(-1)
+
+    def iterunits():
+        for input_file in inputs:
+            with open(input_file, "r") as fp:
+                units = yaml.load(fp)['units']
+                for unit_name, unit_details in units.iteritems():
+                    yield unit_name, unit_details
+
+    if mode == 'def':
         print("""/* This is an AUTO-GENERATED file! Update it with the output of `ctrl-gen.py def`. */
 #include "libuvc/libuvc.h"
 #include "libuvc/libuvc_internal.h"
@@ -297,20 +286,17 @@ static const int REQ_TYPE_SET = 0x21;
 static const int REQ_TYPE_GET = 0xa1;
 """)
         fun = gen_ctrl
-    elif sys.argv[1] == 'decl':
+    elif mode == 'decl':
         fun = gen_decl
-    else:
-        print("Invalid mode {1}".format(*sys.argv))
-        sys.exit(-1)
+    elif mode == 'yaml':
+        exported_units = OrderedDict()
+        for unit_name, unit_details in iterunits():
+            exported_units[unit_name] = export_unit(unit_details)
 
-    for ctrl in CT_CONTROLS:
-        code = fun('CT', *ctrl)
-        print(code)
+        yaml.dump({'units': exported_units}, sys.stdout, default_flow_style=False)
+        sys.exit(0)
 
-    for ctrl in PU_CONTROLS:
-        code = fun('PU', *ctrl)
-        print(code)
-
-    for ctrl in SU_CONTROLS:
-        code = fun('SU', *ctrl)
-        print(code)
+    for unit_name, unit_details in iterunits():
+        for control_name, control_details in unit_details['controls'].iteritems():
+            code = fun(unit_name, unit_details, control_name, control_details)
+            print(code)
